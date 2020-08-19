929aa9e2501a9a382cfcb36079d7da2da702e1dc
Jacob <Jacob.D.Christiansen@gmail.com>
2020-08-19 11:26:27 -0700

    created 2 test PPM

---
+++ README.md
@@ -0,0 +1,7 @@
+# Authors
+Daniel Mercado and Jacob Christiansen 
+
+# Usage
+Used to read in a PPM file of type p3 or p6, convert in to either type p3 or p6 and then print back to the screen the new PPM file.
+
+# Known Issues
--- ppmrw.c
+++ ppmrw.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    
-}
-
 void fail(char *mess)
 {
     printf("An error has occured:\n");
-    prinff("\t");
+    printf("\t%s\n", mess);
+    printf("\n");
+    exit(1);
+} 
+
+void printPPM(char *file)
+{
+    FILE *fptr = fopen(filename, mode);
 }
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        fail("Wrong Number of Arguments\n\tPlease input a file path");
+    }
+    
+    char *inPPM = argv[1];
+    printPPM(inPPM);
+}
+
+
+
